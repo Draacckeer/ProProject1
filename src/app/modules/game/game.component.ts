90161f1bc5f1b13d9544ea85284435dcedc0e651
Draacckeer <u201922975@upc.edu.pe>
2022-05-31 02:50:21 -0500

    Add Game Component Logic

--- src/app/modules/game/game.component.ts
+++ src/app/modules/game/game.component.ts
@@ -10,20 +10,30 @@ export class GameComponent{
   operators: String[] = ["+", "-", "*", "÷"];
   currentLine = 0;
 
+  problems: number[] = [];
+
   answers: any = [];
   operatorAnswer: String = "";
 
   constructor() {
+    this.setupVariables();
+  }
+
+  setupVariables(){
+    this.problems = [];
+    this.problems.push(Math.floor(Math.random() * 2) + 1);
+    this.problems.push(Math.floor(Math.random() * 2) + this.problems[0] + 1);
+    this.currentLine = 0;
   }
 
   submitAnswer(){
     if(this.currentLine==0){
-      if(this.answers[0]=="x"&&this.answers[1]==5&&this.operatorAnswer=='-'&&this.answers[2]==2){
+      if(this.answers[0]=="x"&&this.answers[1]==this.problems[1]&&this.operatorAnswer=='-'&&this.answers[2]==this.problems[0]){
         this.currentLine++;
       }
     }
     else if(this.currentLine==1){
-      if(this.answers[3]=='x'&&this.answers[4]==3){
+      if(this.answers[3]=='x'&&this.answers[4]==this.problems[1]-this.problems[0]){
         this.currentLine++;
       }
     }
